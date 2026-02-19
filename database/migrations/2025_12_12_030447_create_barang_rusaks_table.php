04060dc76b9929a009e6a399086a6a2f17c002c1
iamauschwitz12 <iamauschwitz@gmail.com>
2026-02-19 10:03:26 +0700

    fix barang rusak

--- database/migrations/2025_12_12_030447_create_barang_rusaks_table.php
+++ database/migrations/2025_12_12_030447_create_barang_rusaks_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,8 +14,9 @@ return new class extends Migration
             $table->id();
             $table->foreignId('ruang_id')->constrained('ruangs')->cascadeOnDelete();
             $table->foreignId('lantai_id')->constrained('lantais')->cascadeOnDelete();
-            $table->foreignId('tipe_aset_id')->constrained('tipe_asets')->cascadeOnDelete();
-            $table->morphs('inventaris'); // inventaris_id + inventaris_type
+            $table->foreignId('tipe_aset_id')->nullable()->constrained('tipe_asets')->nullOnDelete();
+            $table->string('inventaris_type')->nullable(); // App\Models\Sekolah, etc.
+            $table->json('inventaris_id')->nullable();     // array of IDs
             $table->unsignedInteger('jumlah_rusak');
             $table->date('tgl_rusak');
             $table->date('tgl_input');
